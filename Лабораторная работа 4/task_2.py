ca02b1d035deaa497512310567f26bd14bc804ea
BagovaMYu <115161138+BagovaMYu@users.noreply.github.com>
2022-10-22 12:20:06 +0300

    Лабораторная работа 4

---
+++ Лабораторная работа 4/task_2.py
@@ -0,0 +1,31 @@
+def get_count_char(str_):
+    str_ = str_.lower()
+    char_dict = {}
+    for char in str_:
+        if char.isalpha():
+            if char in char_dict:
+                char_dict[char] += 1
+            else:
+                char_dict[char] = 1
+
+    return char_dict
+
+main_str = """
+    Данное предложение будет разбиваться на отдельные слова. 
+    В качестве разделителя для встроенного метода split будет выбран символ пробела. На выходе мы получим список отдельных слов. 
+    Далее нужно отсортировать слова в алфавитном порядке, а после сортировки склеить их с помощью метода строк join. Приступим!!!!
+"""
+
+print(get_count_char(main_str))
+
+
+char_dict_percent = get_count_char(main_str)
+
+def get_percent(char_dict):
+    total_char = sum(char_dict.values())
+    for current_char in char_dict:
+        char_dict[current_char] = char_dict[current_char] * 100 / total_char
+
+    return char_dict
+
+#print(get_percent(char_dict_percent))
